28e730f0ce39569ab6485b69fdaec9f72e18bedb
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2015-10-16 23:54:10 +0200

    Cleanup prompt setting and incorporate __git_ps1

--- .bashrc.d/prompt.sh
+++ .bashrc.d/prompt.sh
@@ -1,8 +1,30 @@
-case "$TERM" in
-xterm*|rxvt*)
-    PROMPT_COMMAND='history -a; echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD/$HOME/~}\007"'
-    ;;
-*)
-    PROMPT_COMMAND='history -a'
-    ;;
-esac
+if declare -f __git_ps1 > /dev/null; then
+    GIT_PS1_SHOWDIRTYSTATE=1
+    GIT_PS1_SHOWUPSTREAM=auto
+    GIT_PS1_DESCRIBE_STYLE=branch
+    GIT_PS1_SHOWCOLORHINTS=1
+    PS1_GIT=1
+fi
+
+__prompt() {
+    history -a
+    case "$TERM" in
+    xterm*|rxvt*)
+        echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD/$HOME/\~}\007"
+        ;;
+    esac
+    if [ $USER  == 'root' ]; then
+        USERX="\033[31;1m$USER\033[0m"
+        PROMPT='# '
+    else
+        USERX=$USER
+        PROMPT='$ '
+    fi
+    PS1="$USERX@$HOSTNAME:${PWD/$HOME/\~}"
+    if [ "$PS1_GIT" == 1 ]; then
+        __git_ps1 "$PS1" "$PROMPT"
+    else
+        PS1="$PS1$PROMPT"
+    fi
+}
+PROMPT_COMMAND=__prompt
