163687a10b57080faffa408ef1068858884c0ba9
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2015-10-17 13:11:47 +0200

    Auto-fix user.email in work git repos

--- .bashrc.d/cd.sh
+++ .bashrc.d/cd.sh
@@ -1,5 +1,9 @@
 __last_repo=
 __cd() {
+    __maybe_fetch
+    __fix_git_config
+}
+__maybe_fetch() {
     # If changed to a different repo, fetch
     __repo="$(git rev-parse --show-toplevel 2>/dev/null)"
     case "$__repo,$__last_repo" in
@@ -8,8 +12,19 @@ __cd() {
     *)
         __last_repo=$__repo
         set +m
-        git fetch -q --all &
+        git fetch -q --all 2>/dev/null &
         ;;
     esac
 }
+__fix_git_config() {
+    case "$(git config remote.origin.url),$(git config user.email)" in
+    *booking.com*,*booking.com*)
+        ;;
+    *booking.com*,*)
+        echo "Fixing user configuration"
+        git config user.email dennis.kaarsemaker@booking.com
+        ;;
+    esac
+}
+
 CD_COMMAND=__cd
