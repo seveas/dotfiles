06e649ad3178788e55fd3ad9e5714d48ddc74b4f
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2015-10-18 19:49:47 +0200

    Make the cow silent

--- .bashrc.d/cow.sh
+++ .bashrc.d/cow.sh
@@ -1,6 +1,6 @@
 cow() {
-    which cowsay > /dev/null || return
-    which fortune > /dev/null || return 
+    which cowsay > /dev/null 2>&1 || return
+    which fortune > /dev/null 2>&1 || return
     cow="$(ls /usr/share/cowsay/cows | shuf -n 1)"
     fortune -s | cowsay -n -f "$cow"
 }
