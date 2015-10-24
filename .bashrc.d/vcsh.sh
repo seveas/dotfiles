5bb1055b33fbdfa4420f84b30f4f390d6a17c393
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2015-10-24 09:02:28 +0200

    Only fetch vcsh updates if we have a key

--- .bashrc.d/vcsh.sh
+++ .bashrc.d/vcsh.sh
@@ -2,7 +2,9 @@ if [ -e ~/.dotfiles ]; then
     PATH=$PATH:~/.dotfiles/bin
 fi
 if [ $(stat -c %Y $(vcsh dotfiles rev-parse --git-dir)) -lt $(( $(date +%s) - 3600 )) ]; then
-    vcsh dotfiles fetch
+    if ssh-add -l >/dev/null 2>&1; then
+        vcsh dotfiles fetch
+    fi
 fi
 if [ -n "$(vcsh dotfiles status --porcelain)" ]; then
     VCSH="*"
