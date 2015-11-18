a314170e729df4c28eb2da19138ca8c6e7e5eb8b
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2015-11-18 14:23:41 +0100

    Don't clobber repo configs, don't auto-fetch work repos

--- .bashrc.d/cd.sh
+++ .bashrc.d/cd.sh
@@ -1,33 +1,28 @@
 test -n "$SUDO_USER" && return
 __last_repo=
 __cd() {
-    __maybe_fetch
-    __fix_git_config
+    __git_update
 }
-__maybe_fetch() {
-    # If changed to a different repo, fetch
-    __repo="$(git rev-parse --show-toplevel 2>/dev/null)"
-    case "$__repo,$__last_repo" in
-    ,*|"$__repo,$__repo")
-        ;;
-    *)
-        __last_repo=$__repo
-        git fetch -q --all 2>/dev/null &
-        ;;
-    esac
-}
-__fix_git_config() {
+__git_update() {
+     __repo="$(git rev-parse --show-toplevel 2>/dev/null)"
     case "$(git config remote.origin.url),$(git config user.email)" in
     *booking.com*,*booking.com*)
         ;;
     *booking.com*,*)
-        echo "Fixing user configuration"
-        git config user.email dennis.kaarsemaker@booking.com
+        export GIT_AUTHOR_MAIL=dennis.kaarsemaker@booking.com
+        export GIT_COMMITTER_MAIL=dennis.kaarsemaker@booking.com
         ;;
+    *)
+        unset GIT_AUTHOR_MAIL
+        unset GIT_COMMITTER_MAIL
+        if [ "$__repo" != "$__last_repo" ]; then
+            __last_repo=$__repo
+            ( set +m; git fetch -q --all 2>/dev/null & )
+        fi
     esac
 }
 
 cd() {
     command cd "$@"
-    ( set +m; __cd )
+    __cd
 }
