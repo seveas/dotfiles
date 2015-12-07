cf8d0516483989ed42cd64b9e3bcf5108b4e825e
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2015-12-07 22:08:20 +0100

    Let the vcsh prompt update itself

--- .bashrc.d/vcsh.sh
+++ .bashrc.d/vcsh.sh
@@ -2,6 +2,7 @@ if [ -e ~/.dotfiles ]; then
     PATH=$PATH:~/.dotfiles/bin
 fi
 test -n "$SUDO_USER" && return
+unset vcsh
 if [ $(stat -c %Y "$(vcsh dotfiles rev-parse --git-dir)"/FETCH_HEAD) -lt $(( $(date +%s) - 3600 )) ]; then
     if ssh-add -l >/dev/null 2>&1; then
         if /sbin/route -n | grep -q UG; then
@@ -23,3 +24,16 @@ else
         vcsh dotfiles merge --ff --ff-only '@{u}'
     fi
 fi
+vcsh() {
+    command vcsh "$@"
+    VCSH=
+    if [ -n "$(command vcsh dotfiles status --porcelain)" ]; then
+        VCSH="*"
+    fi
+    if [ $(command vcsh dotfiles rev-list --count '@{u}..') != 0 ]; then
+        VCSH="$VCSH>"
+    fi
+    if [ -n "$VCSH" ]; then
+        VCSH="\033[31;1m$VCSH\033[0m "
+    fi
+}
