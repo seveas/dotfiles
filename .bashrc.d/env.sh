bd218f5340131c07df1f2b137b36ab70f7053d49
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2015-12-15 09:41:48 +0100

    Set up go environment properly

--- .bashrc.d/env.sh
+++ .bashrc.d/env.sh
@@ -2,12 +2,12 @@ export EDITOR=vim
 export VISUAL=vim
 export PAGER=less
 export LESS='-MQCFSX'
-export PATH=~/bin:$PATH:~/code/hacks
+export PATH=~/bin:~/go/bin/:$PATH:~/code/hacks
 export HISTCONTROL=ignoreboth
 export HISTSIZE=10000
 export PERL5LIB=~/perl/lib/perl5:$PERL5LIB
-export GOROOT=~/go
 export EMAIL=dennis@kaarsemaker.net
+export GOPATH=~/go
 export DEBEMAIL=$EMAIL
 if [ "$TERM" != "dumb" ]; then
     eval "`dircolors -b`"
