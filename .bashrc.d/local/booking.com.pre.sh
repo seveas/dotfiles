a06949c751a1f8d736f1ca86e65018b4f5b1ef12
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2016-01-02 19:59:24 +0100

    Fix proxy settings

--- .bashrc.d/local/booking.com.pre.sh
+++ .bashrc.d/local/booking.com.pre.sh
@@ -6,7 +6,7 @@ export http_proxy=$http_proxy
 export https_proxy=$http_proxy
 export ftp_proxy=$http_proxy
 export rsync_proxy=$http_proxy
-export no_proxy="*.booking.com *.activehotels.net *.activehotels.com"
+export no_proxy="*.booking.com"
 
 for role in $SERVERDB_ROLE_NAMES; do
     if [ -e ~/.bashrc.d/local/$role.$phase.sh ]; then
