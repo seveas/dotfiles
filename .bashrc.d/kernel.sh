c59d886952d1d6c16f8f1995204d6b7ae01cb71f
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2016-02-01 22:56:19 +0100

    Add kernel cleanup script

---
+++ .bashrc.d/kernel.sh
@@ -0,0 +1,11 @@
+kernel-cleanup() {
+    test -x /usr/bin/dpkg || { echo "Not a debian-ish system"; return 1; }
+    latest=$(apt-cache depends linux-image-generic | sed -ne 's/.*Depends: linux-image-\([1-9][^ ]*\)/\1/p')
+    running=$(uname -r)
+    toremove=$(
+        dpkg -l linux-image-[0-9]* linux-headers-[0-9]* |
+        awk '/^ii/{print $2}' |
+        grep -v "\(${running%%-generic}\|${latest%%-generic}\)"
+    )
+    test -n "$toremove" && sudo apt-get remove --purge $toremove
+}
