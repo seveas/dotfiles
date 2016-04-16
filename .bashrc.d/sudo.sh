254f4b9b950c8bcf08c51663ed5d78d1bcd2083b
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2016-04-16 20:24:54 +0200

    Don't let the sudo wrapper break netstat -i
    It stupidly replaced -i anywhere on the command line. It no longer does.

--- .bashrc.d/sudo.sh
+++ .bashrc.d/sudo.sh
@@ -3,14 +3,28 @@ sudo() {
     declare -a args
     declare -a final_args
     while [ "$#" != 0 ]; do
-        if [ "$1" = '-i' ]; then
-            final_args+=('-H'  "$SHELL"  "--rcfile"  "$sudohome/.bashrc")
-        else
-            args+=($1)
-        fi
-        shift
+        case "$1" in
+            -i|--login)
+                final_args+=('-H'  "$SHELL"  "--rcfile"  "$sudohome/.bashrc")
+                shift
+                ;;
+            -C|--close-from|-p|--prompt|-r|--role|-t|--type|-U|--other-user|-u|--user)
+                args+=("$1" "$2")
+                shift; shift
+                ;;
+            --)
+                break
+                ;;
+            -*)
+                args+=("$1")
+                shift
+                ;;
+            *)
+                break
+                ;;
+        esac
     done
-    command sudo "${args[@]}" "${final_args[@]}"
+    command sudo "${args[@]}" "${final_args[@]}" "$@"
 }
 if [ -n "$SUDO_USER" ]; then
     alias vim="vim -u $sudohome/.vimrc"
