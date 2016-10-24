330028371e879720db8cb9b3c684b790465da7c8
Dennis Kaarsemaker <dennis.kaarsemaker@booking.com>
2016-10-24 13:46:09 +0200

    KVM owner identifier changed

--- .bashrc.d/local/dev.booking.com.post.sh
+++ .bashrc.d/local/dev.booking.com.post.sh
@@ -1,5 +1,5 @@
-if [ ! -e /etc/bookings/DEVKVM_USER ]; then return; fi
-owner="$(</etc/bookings/DEVKVM_USER)"
+if [ ! -e /etc/bookings/DEVKVM ]; then return; fi
+owner="$(</etc/bookings/DEVKVM)"
 if [ "$owner" != "dkaarsemaker" ]; then return; fi
 myrepo=/usr/local/git_tree/main
 test -e $myrepo && cd $myrepo
