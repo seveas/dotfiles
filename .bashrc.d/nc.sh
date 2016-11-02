4584c7f3626f9935a37ffa9d0065dbf2873d0916
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2016-11-02 16:17:44 +0100

    Not all whiches are equal...

--- .bashrc.d/nc.sh
+++ .bashrc.d/nc.sh
@@ -1,6 +1,6 @@
 # Use openbsd's nc if it's available and the system nc isn't the openbsd nc
 
-case "$(type -t nc.openbsd),$(readlink -f $(which nc))" in
+case "$(type -t nc.openbsd),$(readlink -f $(/bin/which nc))" in
     ,*|*,*openbsd*)
         ;;
     file,*)
