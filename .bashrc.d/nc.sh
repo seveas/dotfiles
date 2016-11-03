e426a609c936708d79922def3acc7514e8dbf868
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2016-11-03 16:01:44 +0100

    Of cource centos stores which somewhere else

--- .bashrc.d/nc.sh
+++ .bashrc.d/nc.sh
@@ -1,6 +1,7 @@
 # Use openbsd's nc if it's available and the system nc isn't the openbsd nc
 
-case "$(type -t nc.openbsd),$(readlink -f $(/bin/which nc))" in
+unalias nc 2>/dev/null
+case "$(type -t nc.openbsd),$(readlink -f $(which nc))" in
     ,*|*,*openbsd*)
         ;;
     file,*)
