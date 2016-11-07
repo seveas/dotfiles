ec41d8b527131b9317e412d07c72e622e57d0f62
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2016-11-07 11:33:11 +0100

    Show kvm owners

--- .bashrc.d/local/dev.booking.com.post.sh
+++ .bashrc.d/local/dev.booking.com.post.sh
@@ -1,5 +1,8 @@
 if [ ! -e /etc/bookings/DEVKVM ]; then return; fi
 owner="$(</etc/bookings/DEVKVM)"
-if [ "$owner" != "dkaarsemaker" ]; then return; fi
+if [ "$owner" != "dkaarsemaker" ]; then
+    echo "This is $(</etc/bookings/DEVKVM)-$(</etc/bookings/SERVER_ROLE)"
+    return;
+fi
 myrepo=/usr/local/git_tree/main
 test -e $myrepo && cd $myrepo
