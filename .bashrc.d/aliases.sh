e48d5bca4efae4462b31b5c752ee83802a8a3cd0
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2017-05-02 13:31:13 +0200

    Ldapsearch needs an -LLL

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -14,7 +14,7 @@ alias ntoa='python -c "import sys,socket,struct; print(socket.inet_ntoa(struct.p
 alias today='echo $(($(date +%s)/86400))'
 alias lscg=systemd-cgls
 alias dig='dig +noall +answer'
-alias ldapsearch='ldapsearch -o ldif-wrap=no -x'
+alias ldapsearch='ldapsearch -o ldif-wrap=no -x -LLL'
 alias ldapdecode='awk '\''BEGIN{FS=":: ";c="base64 -d"}{if(/\w+:: /) {print $2 |& c; close(c,"to"); c |& getline $2; close(c); printf("%s:: \"%s\"\n", $1, $2); next} print $0 }'\'''
 alias puppet="sudo puppet agent -t"
 alias my-puppet="sudo puppet agent -t --environment=dkaarsemaker"
