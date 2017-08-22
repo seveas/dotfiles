c849c7a5fe9aa6244139a3acdad8c31c298791c1
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2017-08-22 11:22:06 +0200

    Force myself to use more modern python :)

--- .bashrc.d/python.sh
+++ .bashrc.d/python.sh
@@ -1,10 +1,20 @@
 export PYTHONPATH=/home/dennis/lib/python
 export PYTHONSTARTUP=$HOME/.startup.py
-from() { expect -c "spawn -noecho python
-expect \">>> \"
-send \"from $*\r\"
-interact +++ return"; }
-import() { expect -c "spawn -noecho python
-expect \">>> \"
-send \"import $*\r\"
-interact +++ return"; }
+PYTHONS=(
+    /usr/local/bin/blue-python3.5
+    /usr/local/bin/blue-python3.4
+    /usr/local/bin/blue-python3.6
+    /usr/bin/python3
+    /bin/python3
+    /usr/local/bin/blue-python2.7
+    /usr/bin/python2
+    /bin/python2
+    /usr/bin/python
+    /bin/python
+)
+python() {(
+    for py in $PYTHONS; do
+        test -x $py && exec $py "$@"
+        break
+    done
+)}
