83bff1ace43a472da645755bf369d10a4af2ce4c
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2017-10-25 17:28:56 +0200

    Jawohl!

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -18,3 +18,4 @@ alias ldapsearch='ldapsearch -o ldif-wrap=no -x -LLL'
 alias ldapdecode='awk '\''BEGIN{FS=":: ";c="base64 -d"}{if(/\w+:: /) {print $2 |& c; close(c,"to"); c |& getline $2; close(c); printf("%s:: \"%s\"\n", $1, $2); next} print $0 }'\'''
 alias puppet="sudo puppet agent -t"
 alias my-puppet="sudo puppet agent -t --environment=dkaarsemaker"
+alias ßh=ssh
