cd4ebf6461b17b6de5f22ab99a5677fdf2dd6e7d
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2017-11-16 10:58:11 -0800

    Reinstate work-specific git mail, but now for GitHub

--- .bashrc.d/cd.sh
+++ .bashrc.d/cd.sh
@@ -5,6 +5,21 @@ __cd() {
     __git_update
 }
 __git_update() {
+    case "$(git config remote.origin.url),$(git config user.email)" in
+    *github.com/github*,*github.com*)
+        ;;
+    *github.com/github*,*)
+        export GIT_AUTHOR_EMAIL=seveas@github.com
+        export GIT_COMMITTER_EMAIL=seveas@github.com
+        ;;
+    *github.com:github*,*)
+        export GIT_AUTHOR_EMAIL=seveas@github.com
+        export GIT_COMMITTER_EMAIL=seveas@github.com
+        ;;
+    *)
+        unset GIT_AUTHOR_EMAIL
+        unset GIT_COMMITTER_EMAIL
+    esac
     __repo="$(git rev-parse --show-toplevel 2>/dev/null)"
     if [ "$__repo" != "$__last_repo" ]; then
         __last_repo=$__repo
