b9a0046c87aa26d0b5661b2f776a5df74bbe9638
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2017-12-11 09:42:05 +0100

    Load the git prompt early enough

--- .bashrc.d/local/Darwin.post.sh
+++ .bashrc.d/local/Darwin.post.sh
@@ -3,6 +3,5 @@ export PYTHONPATH=~/Library/Python/2.7/lib:$PYTHONPATH
 export LC_ALL=en_US.UTF-8
 export LC_TIME=nl_NL.UTF-8
 
-. /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh
 . /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-completion.bash
 eval "$(rbenv init -)"
--- .bashrc.d/local/Darwin.pre.sh
+++ .bashrc.d/local/Darwin.pre.sh
@@ -1,1 +1,2 @@
 (ssh-add -l || ssh-add -K )&>/dev/null
+. /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh
