26e4174907ca061d86af594fa14eedd0ca79a6a2
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2018-04-04 14:16:06 +0200

    Work git config tweaks

--- .bashrc.d/local/Darwin.post.sh
+++ .bashrc.d/local/Darwin.post.sh
@@ -2,6 +2,8 @@ export PATH=~/Library/Python/2.7/bin:$PATH
 export PYTHONPATH=~/Library/Python/2.7/lib:$PYTHONPATH
 export LC_ALL=en_US.UTF-8
 export LC_TIME=nl_NL.UTF-8
+# I don't want the xcode-enforced credential helper
+export GIT_CONFIG_NOSYSTEM=1
 
 . /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-completion.bash
 eval "$(rbenv init -)"
