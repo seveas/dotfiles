c5f9e53a75c811f12a0007546491069f8ccc6731
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2020-04-28 09:00:31 +0200

    Remove obsolet mac python settings

--- .bashrc.d/local/Darwin.post.sh
+++ .bashrc.d/local/Darwin.post.sh
@@ -1,5 +1,3 @@
-export PATH=~/Library/Python/2.7/bin:$PATH
-export PYTHONPATH=~/Library/Python/2.7/lib:$PYTHONPATH
 export LC_ALL=en_US.UTF-8
 export LC_TIME=nl_NL.UTF-8
 # I don't want the xcode-enforced credential helper
