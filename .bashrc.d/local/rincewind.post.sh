853f75f7e9ada270e54937c83164ca14d981e70c
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2020-06-03 14:04:56 +0200

    Host renames, no more glorifying of the us military

---
+++ .bashrc.d/local/rincewind.post.sh
@@ -0,0 +1,3 @@
+export PATH=$PATH:~/kafka_2.11-2.1.0/bin
+alias vpn="osascript -e 'tell application \"Viscosity\" to connect \"github-iad-prod\"'"
+alias gc2_console="ssh -L 39393:127.0.0.1:5900 -L 39394:127.0.0.1:5901 -L 39395:127.0.0.1:5902 -L 39396:127.0.0.1:5903 -L 39397:127.0.0.1:5904 -L 39398:127.0.0.1:5905 -L 39399:127.0.0.1:5906 -L 39400:127.0.0.1:5907 -L 39401:127.0.0.1:5907"
