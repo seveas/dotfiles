1e8d3392fd7d293e508817a5a1b8b6e7dc4896d6
Dennis Kaarsemaker <seveas@github.com>
2020-10-26 21:41:28 +0100

    More vpn's

--- .bashrc.d/local/rincewind.post.sh
+++ .bashrc.d/local/rincewind.post.sh
@@ -1,3 +1,5 @@
 export PATH=$PATH:~/kafka_2.13-2.6.0/bin
-alias vpn="osascript -e 'tell application \"Viscosity\" to connect \"github-iad-prod\"'"
+vpn() {
+    osascript -e "tell application \"Viscosity\" to connect \"${1:-github-iad-prod}\""
+}
 alias gc2_console="ssh -L 39393:127.0.0.1:5900 -L 39394:127.0.0.1:5901 -L 39395:127.0.0.1:5902 -L 39396:127.0.0.1:5903 -L 39397:127.0.0.1:5904 -L 39398:127.0.0.1:5905 -L 39399:127.0.0.1:5906 -L 39400:127.0.0.1:5907 -L 39401:127.0.0.1:5907"
