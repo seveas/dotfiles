ddd34637f4c151cbd8b33e51d2fbf43ec8f1d43f
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2021-01-04 09:51:12 +0100

    Full path to ssh-add when using -K, so we use the right -K

--- .bashrc.d/local/Darwin.pre.sh
+++ .bashrc.d/local/Darwin.pre.sh
@@ -1,4 +1,4 @@
-(ssh-add -l || ssh-add -K )&>/dev/null
+(ssh-add -l || /usr/bin/ssh-add -K )&>/dev/null
 . /usr/local/etc/bash_completion
 printf() { if [ $1 = "-v" ]; then command printf "$@"; else gprintf "$@"; fi; }
 mkdir -p ~/.shc
