a1ac257970c5ef0875c459e52041490bc7196351
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2024-03-11 13:37:57 +0100

    Add a function to make bundler play nice with github packages

--- .bashrc.d/local/twoflower.post.sh
+++ .bashrc.d/local/twoflower.post.sh
@@ -1,2 +1,6 @@
 alias rr="bundle exec rails runner"
 iterm-badge
+bundle_login() {
+    BUNDLE_RUBYGEMS__PKG__GITHUB__COM="$(gh api /user --jq .login):$(gh auth token)"
+    export BUNDLE_RUBYGEMS__PKG__GITHUB__COM
+}
