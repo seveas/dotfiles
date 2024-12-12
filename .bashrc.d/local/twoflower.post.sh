b578ee45850965929af41deebecc27d5018c438c
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2024-12-12 16:12:55 +0100

    Githubbery

--- .bashrc.d/local/twoflower.post.sh
+++ .bashrc.d/local/twoflower.post.sh
@@ -4,3 +4,6 @@ bundle_login() {
     BUNDLE_RUBYGEMS__PKG__GITHUB__COM="$(gh api /user --jq .login):$(gh auth token)"
     export BUNDLE_RUBYGEMS__PKG__GITHUB__COM
 }
+ghcr_login() {
+    gh auth token | docker login ghcr.io --username "$(gh api /user --jq .login)" --password-stdin
+}
