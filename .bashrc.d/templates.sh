dd9c6870e5829f50d22e4ebf54f4fa81d8eef4ba
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2025-05-22 14:12:53 +0200

    Make it easier to re-render templates

--- .bashrc.d/templates.sh
+++ .bashrc.d/templates.sh
@@ -2,29 +2,33 @@
 
 not_with_sudo
 
-for srcf in $(find ~/.template/ -type f); do
-    dst="${srcf/.template\/}"
-    if [ "$srcf" -nt "$dst" ]; then (
-        content=
-        mode=
-        dirmode=
-        dstdir="$(dirname $dst)"
-        . "$srcf"
-        if [ -n "$content" ]; then
-            if [ ! -e "$dstdir" ]; then
-                if [ -n "$dirmode" ]; then
-                    mkdir -p -m "$dirmode" "$dstdir"
-                else
-                    mkdir -p "$dstdir"
+render_dotfiles_templates() {
+    for srcf in $(find ~/.template/ -type f); do
+        dst="${srcf/.template\/}"
+        if [ "$srcf" -nt "$dst" ]; then (
+            content=
+            mode=
+            dirmode=
+            dstdir="$(dirname $dst)"
+            . "$srcf"
+            if [ -n "$content" ]; then
+                if [ ! -e "$dstdir" ]; then
+                    if [ -n "$dirmode" ]; then
+                        mkdir -p -m "$dirmode" "$dstdir"
+                    else
+                        mkdir -p "$dstdir"
+                    fi
                 fi
+                if [ -e "$dst" ] && [ ! -w "$dst" ]; then
+                    chmod u+w "$dst"
+                fi
+                echo "$content" > "$dst"
             fi
-            if [ -e "$dst" ] && [ ! -w "$dst" ]; then
-                chmod u+w "$dst"
+            if [ -n "$mode" ]; then
+                chmod "$mode" "$dst"
             fi
-            echo "$content" > "$dst"
-        fi
-        if [ -n "$mode" ]; then
-            chmod "$mode" "$dst"
-        fi
-    ) fi
-done
+        ) fi
+    done
+}
+
+render_dotfiles_templates
